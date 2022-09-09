3bf9d624962e253042ed37fde47c7617eb507f7a
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2022-09-09 16:48:36 +0100

    Add public subnets for ALBs

--- terraform/modules/networking/outputs.tf
+++ terraform/modules/networking/outputs.tf
@@ -7,6 +7,11 @@ output "nat_gateway_ip" {
   value = aws_eip.nat_gateway.public_ip
 }
 
+output "public_subnets" {
+  value       = aws_subnet.public_subnet
+  description = "var.number_of_public_subnets public /24 subnets"
+}
+
 output "ad_private_subnets" {
   value = aws_subnet.ad_subnet
 }
--- terraform/modules/networking/subnet.tf
+++ terraform/modules/networking/subnet.tf
@@ -20,6 +20,16 @@ resource "aws_subnet" "ad_subnet" {
   tags                    = var.default_tags
 }
 
+# tfsec:ignore:aws-ec2-no-public-ip-subnet
+resource "aws_subnet" "public_subnet" {
+  count                   = var.number_of_public_subnets
+  cidr_block              = cidrsubnet(aws_vpc.vpc.cidr_block, 8, count.index + 128)
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+  tags                    = merge({ Name = "Public subnet ${count.index}" }, var.default_tags)
+}
+
 resource "aws_subnet" "ad_management_server" {
   availability_zone = data.aws_availability_zones.available.names[0]
   cidr_block        = "10.0.224.0/24"
