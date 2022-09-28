9b2756a4fc56a1c7f872d378019449f8093ebd5c
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2022-09-28 15:58:05 +0100

    Add api and auth.delta domains

--- terraform/modules/dns/main.tf
+++ terraform/modules/dns/main.tf
@@ -9,6 +9,7 @@ data "aws_route53_delegation_set" "main" {
 
 resource "aws_route53_zone" "delegated_zone" {
   name              = var.delegated_domain
+  comment           = "${var.prefix}-delegated-zone"
   delegation_set_id = data.aws_route53_delegation_set.main.id
 }
 
@@ -21,11 +22,10 @@ resource "aws_acm_certificate" "cloudfront_domains_wildcard" {
   provider = aws.us-east-1
 
   domain_name               = "*.${var.primary_domain}"
-  subject_alternative_names = ["*.${var.primary_domain}", "*.${var.delegated_domain}"]
+  subject_alternative_names = ["*.${var.primary_domain}", "*.${var.delegated_domain}", "api.delta.${var.primary_domain}", "auth.delta.${var.primary_domain}"]
   validation_method         = "DNS"
 }
 
-# In practice both of these lists should have length one
 locals {
   delegated_validations     = [for v in aws_acm_certificate.cloudfront_domains_wildcard.domain_validation_options : v if v.domain_name == "*.${var.delegated_domain}"]
   non_delegated_validations = [for v in aws_acm_certificate.cloudfront_domains_wildcard.domain_validation_options : v if v.domain_name != "*.${var.delegated_domain}"]
