7a6b5015e7859f4febf4aa75d9e14de0fbf04b7f
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2022-10-06 11:27:20 +0100

    Add S3 gateway endpoint

---
+++ terraform/modules/networking/s3_gateway.tf
@@ -0,0 +1,56 @@
+data "aws_vpc_endpoint_service" "s3" {
+  service      = "s3"
+  service_type = "Gateway"
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.vpc.id
+  service_name = data.aws_vpc_endpoint_service.s3.service_name
+  policy       = data.aws_iam_policy_document.s3_gateway.json
+  tags = {
+    Name = "s3-gateway-endpoint-${var.environment}"
+  }
+}
+
+resource "aws_vpc_endpoint_route_table_association" "s3" {
+  route_table_id  = aws_route_table.private.id
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+}
+
+# Note that endpoint policies only limit access, credentials are still required to access private buckets
+data "aws_iam_policy_document" "s3_gateway" {
+  statement {
+    sid     = "AmazonLinux2Yum"
+    actions = ["s3:GetObject"]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    resources = [
+      "arn:aws:s3:::amazonlinux.${data.aws_region.current.name}.amazonaws.com/*",
+      "arn:aws:s3:::amazonlinux-2-repos-${data.aws_region.current.name}/*",
+    ]
+  }
+
+  statement {
+    sid     = "ReadBucketsInCurrentAccount"
+    actions = ["s3:GetObject", "s3:ListBucket"]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    resources = [
+      "arn:aws:s3:::*"
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "s3:ResourceAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:PrincipalAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+  }
+}
--- terraform/modules/networking/secrets_manager_interface.tf
+++ terraform/modules/networking/secrets_manager_interface.tf
@@ -1,8 +1,7 @@
 data "aws_vpc_endpoint_service" "secrets_manager" {
-  service = "secretsmanager"
+  service      = "secretsmanager"
+  service_type = "Interface"
 }
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
 
 resource "aws_vpc_endpoint" "secrets_manager" {
   vpc_id            = aws_vpc.vpc.id
@@ -32,6 +31,7 @@ resource "aws_security_group" "secrets_manager_endpoint" {
   }
 }
 
+# Note that endpoint policies only limit access, credentials to access a given secret are still required
 data "aws_iam_policy_document" "secret_manager_endpoint" {
   statement {
     actions = ["secretsmanager:GetSecretValue"]
