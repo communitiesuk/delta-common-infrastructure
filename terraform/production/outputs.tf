197478f6b81209b64d46e715f4d22501ba7ae4cf
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2022-10-06 11:29:38 +0100

    Output subnet ids only for delta subnets

--- terraform/production/outputs.tf
+++ terraform/production/outputs.tf
@@ -1,5 +1,5 @@
-output "delta_internal_subnets" {
-  value = module.networking.delta_internal_subnets
+output "delta_internal_subnet_ids" {
+  value = module.networking.delta_internal_subnets[*].id
 }
 
 output "vpc_id" {
