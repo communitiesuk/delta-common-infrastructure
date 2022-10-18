c3b3f00432e98fa1ec72264c7da531a5a2224750
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2022-10-18 14:35:39 +0100

    DT-58: Switch MarkLogic to Network load balancer

--- terraform/modules/marklogic/load_balancing.tf
+++ terraform/modules/marklogic/load_balancing.tf
@@ -1,9 +1,8 @@
 resource "aws_lb" "ml_lb" {
-  name                       = "marklogic-lb-${var.environment}"
-  internal                   = true
-  subnets                    = var.private_subnets[*].id
-  security_groups            = [aws_security_group.ml_lb.id]
-  drop_invalid_header_fields = true
+  name               = "marklogic-lb-${var.environment}"
+  load_balancer_type = "network"
+  internal           = true
+  subnets            = var.private_subnets[*].id
 }
 
 resource "aws_lb_target_group" "ml" {
@@ -11,19 +10,15 @@ resource "aws_lb_target_group" "ml" {
 
   name                 = "ml-target-${var.environment}-${count.index}"
   port                 = local.lb_ports[count.index]
-  protocol             = "HTTP"
+  protocol             = "TCP"
   vpc_id               = var.vpc.id
   deregistration_delay = 60
+  preserve_client_ip   = true
   health_check {
     interval            = 10 #seconds
     port                = 7997
     unhealthy_threshold = 5
     healthy_threshold   = 5
-  }
-  stickiness {
-    enabled         = true
-    type            = "lb_cookie"
-    cookie_duration = 3600
   }
 }
 
@@ -32,8 +27,7 @@ resource "aws_lb_listener" "ml" {
 
   load_balancer_arn = aws_lb.ml_lb.arn
   port              = aws_lb_target_group.ml[count.index].port
-  # tfsec:ignore:aws-elb-http-not-used
-  protocol = "HTTP"
+  protocol          = "TCP"
 
   default_action {
     type             = "forward"
--- terraform/modules/marklogic/security_groups.tf
+++ terraform/modules/marklogic/security_groups.tf
@@ -1,29 +1,3 @@
-resource "aws_security_group" "ml_lb" {
-  vpc_id      = var.vpc.id
-  description = "MarkLogic LB security group"
-  name        = "ml-lb-sg-${var.environment}"
-
-  dynamic "ingress" {
-    for_each = local.ml_sg_ingress_port_ranges
-    content {
-      from_port   = ingress.value["from_port"]
-      to_port     = ingress.value["to_port"]
-      protocol    = "tcp"
-      cidr_blocks = [var.vpc.cidr_block]
-      description = ingress.value["description"]
-    }
-  }
-
-  # tfsec:ignore:aws-vpc-no-public-egress-sgr
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    description = "Allow all egress"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_security_group" "ml_instance" {
   vpc_id      = var.vpc.id
   description = "MarkLogic Instance security group"
