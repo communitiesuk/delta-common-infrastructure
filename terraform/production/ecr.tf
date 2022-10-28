1151f51c03dc51f6a558e5180bf06eb999c3e903
Hugh Emerson <Hugh.Emerson@softwire.com>
2022-10-28 18:27:07 +0100

    DT-35: small refactor

--- terraform/production/ecr.tf
+++ terraform/production/ecr.tf
@@ -17,25 +17,30 @@ resource "aws_iam_access_key" "delta_ci" {
 }
 
 locals {
-  repositories = [{
-    repo_name = "cpm",
-    push_user = aws_iam_user.cpm_ci.name
-    }, {
-    repo_name = "delta_api",
-    push_user = aws_iam_user.delta_ci.name
-    }, {
-    repo_name = "delta_internal",
-    push_user = aws_iam_user.delta_ci.name
-    }, {
-    repo_name = "delta_fo_to_pdf",
-    push_user = aws_iam_user.delta_ci.name
-  }]
+  repositories = {
+    "cpm" = {
+      repo_name = "cpm",
+      push_user = aws_iam_user.cpm_ci.name
+    },
+    "delta_api" = {
+      repo_name = "delta_api",
+      push_user = aws_iam_user.delta_ci.name
+    },
+    "delta_internal" = {
+      repo_name = "delta_internal",
+      push_user = aws_iam_user.delta_ci.name
+    },
+    "delta_fo_to_pdf" = {
+      repo_name = "delta_fo_to_pdf",
+      push_user = aws_iam_user.delta_ci.name
+    }
+  }
 }
 
 module "ecr" {
-  count  = length(local.repositories)
-  source = "../modules/ecr_repository"
+  for_each = local.repositories
+  source   = "../modules/ecr_repository"
 
-  repo_name = local.repositories[count.index]["repo_name"]
-  push_user = local.repositories[count.index]["push_user"]
+  repo_name = each.value["repo_name"]
+  push_user = each.value["push_user"]
 }
