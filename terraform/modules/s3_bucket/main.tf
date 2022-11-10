20fd690040ae031b3565c1ec29e695c0d12348cf
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2022-11-10 13:13:59 +0000

    DT-28: Add module for S3 bucket with logs

---
+++ terraform/modules/s3_bucket/main.tf
@@ -0,0 +1,117 @@
+variable "bucket_name" {
+  type = string
+}
+
+variable "access_log_bucket_name" {
+  type = string
+}
+
+variable "kms_key_arn" {
+  type        = string
+  default     = null
+  description = "Optional. KMS key to encrypt bucket and access logs bucket."
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.main.arn
+}
+
+resource "aws_s3_bucket" "main" {
+  bucket        = var.bucket_name
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
+  bucket = aws_s3_bucket.main.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = var.kms_key_arn
+      sse_algorithm     = var.kms_key_arn == null ? "AES256" : "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "main" {
+  bucket = aws_s3_bucket.main.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Access logs bucket
+# tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-enable-versioning
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = var.access_log_bucket_name
+}
+
+resource "aws_s3_bucket_logging" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "log/"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = var.kms_key_arn
+      sse_algorithm     = var.kms_key_arn == null ? "AES256" : "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "allow_log_writes" {
+  bucket = aws_s3_bucket.log_bucket.id
+  policy = data.aws_iam_policy_document.allow_log_writes.json
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "allow_log_writes" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["logging.s3.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:PutObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.log_bucket.arn}/*"
+    ]
+
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = [aws_s3_bucket.main.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+  }
+}
