d035b739d346df61707448db409f4fb82613d098
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2022-11-14 17:23:50 +0000

    Import delta ci user

--- terraform/production/ecr.tf
+++ terraform/production/ecr.tf
@@ -11,9 +11,11 @@ resource "aws_iam_access_key" "cpm_ci" {
   user = aws_iam_user.cpm_ci.name
 }
 
-# This user is used by the delta repository and managed there too
-data "aws_iam_user" "delta_ci" {
-  user_name = "delta_ci"
+# This user is used by the delta repo's CI workflows
+# One of a kind
+# tfsec:ignore:aws-iam-no-user-attached-policies
+resource "aws_iam_user" "delta_ci" {
+  name = "delta_ci"
 }
 
 locals {
@@ -24,19 +26,19 @@ locals {
     },
     "delta_api" = {
       repo_name = "delta-api",
-      push_user = data.aws_iam_user.delta_ci.user_name
+      push_user = aws_iam_user.delta_ci.name
     },
     "delta_internal" = {
       repo_name = "delta-internal",
-      push_user = data.aws_iam_user.delta_ci.user_name
+      push_user = aws_iam_user.delta_ci.name
     },
     "delta_fo_to_pdf" = {
       repo_name = "delta-fo-to-pdf",
-      push_user = data.aws_iam_user.delta_ci.user_name
+      push_user = aws_iam_user.delta_ci.name
     },
     "keycloak" = {
       repo_name = "keycloak",
-      push_user = data.aws_iam_user.delta_ci.user_name
+      push_user = aws_iam_user.delta_ci.name
     }
   }
 }
