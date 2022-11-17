b97b244c87f3f7d19c6ca52a9ce3695f25f6176b
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2022-11-17 16:41:58 +0000

    DT-64: Module to create DNS records we manage

---
+++ terraform/modules/dns_records/main.tf
@@ -0,0 +1,25 @@
+variable "records" {
+  type = list(object({
+    record_name  = string
+    record_type  = string
+    record_value = string
+  }))
+}
+
+variable "hosted_zone_id" {
+  type = string
+}
+
+variable "ttl" {
+  type    = number
+  default = 60
+}
+
+resource "aws_route53_record" "records" {
+  for_each = { for r in var.records : r.record_name => r }
+  zone_id  = var.hosted_zone_id
+  name     = each.value.record_name
+  type     = each.value.record_type
+  ttl      = var.ttl
+  records  = [each.value.record_value]
+}
