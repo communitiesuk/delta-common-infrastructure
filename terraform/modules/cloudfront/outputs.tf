a15d72cca40c93e777698c194b7c5a5ab953f742
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2022-11-17 16:54:51 +0000

    DT-64: Delete old modules

--- terraform/modules/cloudfront/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "cloudfront_domain_name" {
-  value = aws_cloudfront_distribution.main.domain_name
-}
-
-output "cloudfront_hosted_zone_id" {
-  value = aws_cloudfront_distribution.main.hosted_zone_id
-}
