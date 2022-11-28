5eeb5e623efd0bd18983f2a5d1024fc0a05b30b3
Hugh Emerson <Hugh.Emerson@softwire.com>
2022-11-28 07:09:09 +0000

    DT-32: remove delta-organizations migration scripts because Vinod says those objects are unused

--- manual_scripts/admt/export_orgs.ps1
+++
@@ -1,7 +0,0 @@
-# Not sure this is necessary. There is code that references this set of OUs but it doesn't seem to do much.
-# Skipping this for now and we will see if the staging site works.
-# This command doesn't work because businessCategory is an object, not a string
-
-Get-ADOrganizationalUnit -SearchBase 'OU=Delta-Organizations,DC=dclg-stag-eclaim,DC=local' -Filter 'Name -like "*"' | Select-Object Name, businessCategory, DisplayName | Export-Csv -Path .\delta_orgs.csv -NoTypeInformation
-
-# Remove the Delta-Organizations OU itself from the top of the list.
--- manual_scripts/admt/import_orgs.ps1
+++
@@ -1,6 +0,0 @@
-$orgs = import-csv "delta_orgs.csv"
-
-
-Foreach($org in $orgs){       
-    New-ADOrganizationalUnit -Name $org.Name -Path "OU=DELTA-Organizations,OU=dluhcdata,DC=dluhcdata,DC=local"
-}
