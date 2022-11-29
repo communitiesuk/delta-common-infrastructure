fc2c2b013a8853a0478f520517dfc0e3b8af8817
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2022-11-29 16:16:53 +0000

    DT-129: Make backup folders in S3

--- terraform/modules/marklogic/backup_buckets.tf
+++ terraform/modules/marklogic/backup_buckets.tf
@@ -27,3 +27,19 @@ module "delta_backup_bucket" {
   force_destroy          = true # TODO DT-129 change
   kms_key_arn            = aws_kms_key.ml_backup_bucket_key.arn
 }
+
+# MarkLogic seems to need the "folders" to exist in S3
+resource "aws_s3_object" "delta_content_folder" {
+  bucket = module.delta_backup_bucket.bucket
+  key    = "delta-content/"
+}
+
+resource "aws_s3_object" "security_folder" {
+  bucket = module.delta_backup_bucket.bucket
+  key    = "security/"
+}
+
+resource "aws_s3_object" "payments_content" {
+  bucket = module.cpm_backup_bucket.bucket
+  key    = "payments-content/"
+}
