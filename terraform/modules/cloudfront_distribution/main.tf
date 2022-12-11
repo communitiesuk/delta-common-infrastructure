47769620550b4ce0ea3df40b7c1f56c639e61f34
Hugh Emerson <Hugh.Emerson@softwire.com>
2022-12-11 13:37:54 +0000

    Increase cloudfront origin response timeout

--- terraform/modules/cloudfront_distribution/main.tf
+++ terraform/modules/cloudfront_distribution/main.tf
@@ -48,6 +48,7 @@ resource "aws_cloudfront_distribution" "main" {
       https_port             = 443
       origin_protocol_policy = var.cloudfront_domain == null ? "http-only" : "https-only"
       origin_ssl_protocols   = ["TLSv1.2"]
+      origin_read_timeout    = 60
     }
 
     custom_header {
