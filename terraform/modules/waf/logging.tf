8eb57802738f9809c053e2d77082b8410b7b8ac9
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2022-12-21 10:35:40 +0000

    Update provider to us-east-1 for waf logs

--- terraform/modules/waf/logging.tf
+++ terraform/modules/waf/logging.tf
@@ -4,11 +4,15 @@ variable "log_group_suffix" {
 
 # tfsec:ignore:aws-cloudwatch-log-group-customer-key
 resource "aws_cloudwatch_log_group" "main" {
+  provider = aws.us-east-1
+
   name              = "aws-waf-logs-${var.log_group_suffix}"
   retention_in_days = 30
 }
 
 resource "aws_wafv2_web_acl_logging_configuration" "main" {
+  provider = aws.us-east-1
+
   log_destination_configs = [aws_cloudwatch_log_group.main.arn]
   resource_arn            = aws_wafv2_web_acl.waf_acl.arn
 
