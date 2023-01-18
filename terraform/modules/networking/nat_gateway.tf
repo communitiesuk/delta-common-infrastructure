c4ff549c2164f251e5e21e7f4cc0ed5164c8e43d
Gregory Moss <gregory.moss@softwire.com>
2023-01-18 17:23:09 +0000

    DT-63 Shield Advanced - protect NAT gateway IPs

--- terraform/modules/networking/nat_gateway.tf
+++ terraform/modules/networking/nat_gateway.tf
@@ -2,6 +2,11 @@ resource "aws_eip" "nat_gateway" {
   vpc = true
 }
 
+resource "aws_shield_protection" "nat_gateway" {
+  name         = "NAT gateway Elastic IP Protection"
+  resource_arn = "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:eip-allocation/${aws_eip.nat_gateway.id}"
+}
+
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_gateway.id
   subnet_id     = aws_subnet.nat_gateway.id
