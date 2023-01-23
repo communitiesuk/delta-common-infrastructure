8dd685fa6ae9736ccf28a6ce380ceafc2a22bde4
Hugh Emerson <Hugh.Emerson@softwire.com>
2023-01-23 17:27:13 +0000

    Allow traffic from VPC to JasperSoft server

--- terraform/modules/jaspersoft/instance.tf
+++ terraform/modules/jaspersoft/instance.tf
@@ -29,9 +29,9 @@ resource "aws_security_group_rule" "jaspersoft_server_http_ingress" {
   from_port                = 8080
   to_port                  = 8080
   protocol                 = "tcp"
-  source_security_group_id = var.public_alb.security_group_id
-  description              = "HTTP on 8080 from ALB"
+  description              = "HTTP on 8080 from within VPC"
   security_group_id        = aws_security_group.jaspersoft_server.id
+  cidr_blocks              = [ var.vpc.cidr_block ]
 }
 
 data "aws_region" "current" {}
