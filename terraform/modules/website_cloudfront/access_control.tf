b1bb4f32f47f39ddf4ddaa9a98816ca5f44ff06e
JBsoftwire <34240850+JBsoftwire@users.noreply.github.com>
2023-02-10 16:40:49 +0000

    Apply suggestions from code review
    Co-authored-by: Ben Ramchandani <35066342+BenRamchandani@users.noreply.github.com>

--- terraform/modules/website_cloudfront/access_control.tf
+++ terraform/modules/website_cloudfront/access_control.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudfront_origin_access_control" "s3" {
   name                              = "${var.prefix}s3-control"
-  description                       = "Access control for the s3 bucket"
+  description                       = "Access control for the static error pages s3 bucket"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
