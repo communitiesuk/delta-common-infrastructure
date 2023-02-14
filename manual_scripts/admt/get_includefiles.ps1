45ff1b2f714e45e0dee57f456936009d484ae99c
Hugh Emerson <Hugh.Emerson@softwire.com>
2023-02-14 18:06:03 +0000

    DT-114: update comment in admt script

--- manual_scripts/admt/get_includefiles.ps1
+++ manual_scripts/admt/get_includefiles.ps1
@@ -1,11 +1,13 @@
-# This file generates a list of entities you want to migrate, known as an "include file".
-# We just list the security groups to migrate and tick the box to include migrating any members.
+# This script generates lists of entities you want to migrate, known as an "include file".
+# It generates two lists of groups (to be imported to different locations) and a list of "registration requests"
 $sourceBase = "DC=datamart,DC=local"
 $server = "datamart.local"
 
 # Migrate groups:
+# To use the file run a group migration and tick the box to include migrating any members.
 Get-ADGroup -SearchBase "OU=Groups,$sourceBase" -Filter 'Name -like "datamart-delta*"' -Server $server | Select-Object @{n='SourceName';e={$_.SamAccountName}} | Export-Csv -Path .\groups-includefile.csv -NoTypeInformation
 Get-ADGroup -SearchBase "CN=datamart-delta,OU=Groups,$sourceBase" -Filter * -Server $server | Select-Object @{n='SourceName';e={$_.SamAccountName}} | Export-Csv -Path .\nested-groups-includefile.csv -NoTypeInformation
 
 # Migrate DeltaRegistrationRequests
+# To use it, run a user migration
 Get-AdUser -SearchBase "CN=DeltaRegistrationRequests,CN=Users,$sourceBase" -Filter *  -Server $server | Select-Object @{n='SourceName';e={$_.SamAccountName}} | Export-Csv -Path .\registration-requests-includefile.csv -NoTypeInformation
