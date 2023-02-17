7f0aa6deb8b19b619b2f2f6f3c236247bc91c32f
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2023-02-17 11:17:10 +0000

    Create password policy for AD service users

--- manual_scripts/active_directory/set-password-policy.ps1
+++ manual_scripts/active_directory/set-password-policy.ps1
@@ -3,6 +3,14 @@
 Set-ADFineGrainedPasswordPolicy -Identity CustomerPSO-03 `
   -ComplexityEnabled $True -LockoutDuration 00:30:00 -LockoutObservationWindow 00:30:00 `
   -ReversibleEncryptionEnabled $False -MinPasswordAge 1.00:00:00 -MaxPasswordAge 90.00:00:00 `
-  -MinPasswordLength 10 -LockoutThreshold 3 -PasswordHistoryCount 24
+  -MinPasswordLength 10 -LockoutThreshold 3 -PasswordHistoryCount 24 `
+  -Description "Delta User Policy"
 
 Add-ADFineGrainedPasswordPolicySubject -Identity CustomerPSO-03 -Subjects datamart-delta-user
+
+ Set-ADFineGrainedPasswordPolicy -Identity CustomerPSO-01 `
+  -ComplexityEnabled $False -ReversibleEncryptionEnabled $False -LockoutThreshold 0 `
+  -MinPasswordAge 0 -MaxPasswordAge 0 -MinPasswordLength 0 -PasswordHistoryCount 0 `
+  -Description "Service User Policy"
+
+Add-ADFineGrainedPasswordPolicySubject -Identity CustomerPSO-01 -Subjects dluhc-service-users
