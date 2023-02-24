964220f922fe4aa243ea3eaadd115638c4269afe
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2023-02-24 11:13:07 +0000

    Add deploy KMS key to outputs

--- terraform/test/outputs.tf
+++ terraform/test/outputs.tf
@@ -142,3 +142,7 @@ output "alarms_sns_topic_arn" {
 output "alarms_sns_topic_global_arn" {
   value = module.notifications.alarms_sns_topic_global_arn
 }
+
+output "deploy_user_kms_key_arn" {
+  value = module.marklogic.deploy_user_kms_key_arn
+}
