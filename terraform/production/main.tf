d5e1d4921fbbee45bbe4ef5d1ea11f3e2a4f4536
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2023-02-27 12:21:32 +0000

    Remove prod ip allowlist

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -263,8 +263,6 @@ module "cloudfront_distributions" {
       aliases             = ["delta.${var.secondary_domain}", "delta.${var.primary_domain}"]
       acm_certificate_arn = module.ssl_certs.cloudfront_certs["delta"].arn
     }
-    # TODO MIGRATION: Remove
-    ip_allowlist                       = local.cloudfront_ip_allowlists.delta_website
     geo_restriction_countries          = ["GB", "IE"]
     origin_read_timeout                = 180 # Required quota increase
     error_rate_alarm_threshold_percent = 5
