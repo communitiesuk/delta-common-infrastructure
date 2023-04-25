b32506d4a69e64a563e8a5af5953e7e1e837f91d
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2023-04-25 09:27:43 +0100

    Export AWS_REGION in ML patch script

--- terraform/modules/marklogic/patch.sh
+++ terraform/modules/marklogic/patch.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 echo "Starting patch script at $(date --iso-8601=seconds)"
 
+export AWS_REGION=${AWS_REGION}
 TOKEN=`curl -sS -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 INSTANCE_ID=`curl -sS -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id`
 AUTOSCALING_GROUP_NAME=`aws autoscaling describe-auto-scaling-instances --instance-ids $INSTANCE_ID --query 'AutoScalingInstances[0].AutoScalingGroupName' --output text`
