4ab418e191d70f0ca86e6513a3cf7e2065c21a17
Jonathan Bloxsom <Jonathan.Bloxsom@softwire.com>
2023-05-05 14:44:48 +0100

    DT-199 scripts for bastion sshd config

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -108,6 +108,18 @@ module "bastion" {
     # Configure SSH banner:
     echo "Legal Warning - Private System! This system and the data within it are private property. Access to the system is only available for authorised users and for authorised purposes. Unauthorised entry contravenes the Computer Misuse Act 1990 of the United Kingdom and may incur criminal penalties as well as damages." > /etc/ssh/banner
     sed -i 's-#Banner none-Banner /etc/ssh/banner-g' /etc/ssh/sshd_config
+
+    sed -i 's/#*LogLevel [A-Za-z]*/LogLevel VERBOSE/' /etc/ssh/sshd_config
+    sed -i 's/#*MaxAuthTries [0-9]*/MaxAuthTries 3/' /etc/ssh/sshd_config
+    sed -i 's/#*MaxSessions [0-9]*/MaxSessions 2/' /etc/ssh/sshd_config
+    sed -i 's/#*AllowAgentForwarding [A-Za-z]*/AllowAgentForwarding no/' /etc/ssh/sshd_config
+    sed -i 's/#*AllowTcpForwarding [A-Za-z]*/AllowTcpForwarding no/' /etc/ssh/sshd_config
+    sed -i 's/#*X11Forwarding [A-Za-z]*/X11Forwarding no/' /etc/ssh/sshd_config
+    sed -i 's/#*TCPKeepAlive [A-Za-z]*/TCPKeepAlive no/' /etc/ssh/sshd_config
+    sed -i 's/#*Compression [A-Za-z]*/Compression no/' /etc/ssh/sshd_config
+    sed -i 's/#*ClientAliveCountMax [0-9]*/ClientAliveCountMax 2/' /etc/ssh/sshd_config
+
+    /usr/sbin/sshd -t
     systemctl restart sshd
     EOT
   tags_asg                = var.default_tags
