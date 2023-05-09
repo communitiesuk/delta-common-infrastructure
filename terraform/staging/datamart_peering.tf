965e3e1009364c6902921a442b615fec774ee89a
Jonathan Bloxsom <Jonathan.Bloxsom@softwire.com>
2023-05-09 14:22:00 +0100

    DT-407 remove security group rules and route table entry

--- terraform/staging/datamart_peering.tf
+++ terraform/staging/datamart_peering.tf
@@ -32,38 +32,3 @@ resource "aws_vpc_peering_connection_options" "to_datamart_staging" {
     allow_remote_vpc_dns_resolution = true
   }
 }
-
-# Note this route table is also used by the bastion server
-resource "aws_route" "ad_management_to_datamart_peer" {
-  route_table_id            = data.aws_route_table.ad_management_server.route_table_id
-  destination_cidr_block    = local.datamart_peering_vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.to_datamart_staging.id
-}
-
-resource "aws_security_group_rule" "bastion_to_datamart_peer" {
-  security_group_id = module.bastion.bastion_security_group_id
-  description       = "Bastion to peered VPC"
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = [local.datamart_peering_vpc_cidr]
-}
-
-# The AD management server's SG already allows open egress
-
-# Traffic is allowed back through the ACL using the open_ingress_cidrs variable on the Networking module (see main.tf)
-
-# As recommended here https://docs.aws.amazon.com/directoryservice/latest/admin-guide/ms_ad_tutorial_setup_trust_prepare_mad_between_2_managed_ad_domains.html#tutorial_setup_trust_open_vpc_between_2_managed_ad_domains
-# Presumably egress to the other DCs would be enough, definitely needed for DNS, not sure if anything else
-# tfsec:ignore:aws-vpc-no-public-egress-sgr
-resource "aws_security_group_rule" "domain_controller_egress_to_datamart" {
-
-  security_group_id = module.active_directory.domain_controller_security_group_id
-  description       = "DC Open Egress"
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = [local.datamart_peering_vpc_cidr]
-}
