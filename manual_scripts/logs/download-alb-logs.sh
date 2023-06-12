56bfef0d0287a3d5b6226936ab52a10d2ca64fc0
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2023-06-12 13:26:51 +0100

    ALB logs script make date variable

--- manual_scripts/logs/download-alb-logs.sh
+++ manual_scripts/logs/download-alb-logs.sh
@@ -5,13 +5,15 @@
 DLUHC_ENV=staging
 APP=cpm
 ACCOUNT_ID=486283582667
+# The last part of the S3 prefix which is the date in yyyy/MM/dd/ format
+# Can specify a day or use e.g. "2023/06/" to download all logs for that month
+S3_FOLDER_DATE="2023/06/20/"
 
 FOLDER="${APP}-${DLUHC_ENV}-alb-logs"
 
 mkdir -p "${APP}-alb-logs"
 
-# Change the date in the S3 URI
-aws s3 cp --recursive "s3://${DLUHC_ENV}-${APP}-alb-access-logs/${DLUHC_ENV}-${APP}-alb/AWSLogs/${ACCOUNT_ID}/elasticloadbalancing/eu-west-1/2023/06/" "${FOLDER}/"
+aws s3 cp --recursive "s3://${DLUHC_ENV}-${APP}-alb-access-logs/${DLUHC_ENV}-${APP}-alb/AWSLogs/${ACCOUNT_ID}/elasticloadbalancing/eu-west-1/${S3_FOLDER_DATE}" "${FOLDER}/"
 
 find "${FOLDER}/" -type f -name '*.gz' | xargs gunzip
 
