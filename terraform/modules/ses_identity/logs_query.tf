264369b26a27c59204f4f9cc9ea29d8081b8f960
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2023-06-20 11:05:10 +0100

    Update SES queries

--- terraform/modules/ses_identity/logs_query.tf
+++ terraform/modules/ses_identity/logs_query.tf
@@ -1,19 +1,21 @@
 resource "aws_cloudwatch_query_definition" "ses-deliveries" {
-  name = "${var.environment}/ses-deliveries-tf"
+  name = "${var.environment}/email-deliveries-ses-tf"
 
   log_group_names = [local.log_group_name_delivered]
 
   query_string = <<EOF
 fields @timestamp, mail.destination.0, mail.commonHeaders.subject
+| sort @timestamp desc
 EOF
 }
 
 resource "aws_cloudwatch_query_definition" "ses-problems" {
-  name = "${var.environment}/ses-problems-tf"
+  name = "${var.environment}/email-bounces-ses-tf"
 
   log_group_names = [local.log_group_name_problem]
 
   query_string = <<EOF
-fields @timestamp, mail.destination.0, bounce.bounceSubType, mail.commonHeaders.subject
+fields @timestamp, mail.destination.0, bounce.bounceType, bounce.bounceSubType, mail.commonHeaders.subject
+| sort @timestamp desc
 EOF
 }
