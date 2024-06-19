174fa260303c930cf725631b119e5d70308b1d7e
Yousuf Desai <119860263+YD-DLUHC@users.noreply.github.com>
2024-06-19 09:58:25 +0100

    DT-753: Replace GitHub Actions access keys with OIDC

--- terraform/modules/github_actions_dev_terraform_roles/policies.tf
+++ terraform/modules/github_actions_dev_terraform_roles/policies.tf
@@ -74,7 +74,8 @@ data "aws_iam_policy_document" "github_actions_terraform_admin_assume_role" {
       values = [
         "repo:communitiesuk/delta-common-infrastructure:environment:${var.environment}",
         "repo:communitiesuk/delta:environment:${var.environment}",
-        "repo:communitiesuk/delta-auth-service:environment:${var.environment}"
+        "repo:communitiesuk/delta-auth-service:environment:${var.environment}",
+        "repo:communitiesuk/common-payments-module:environment:${var.environment}"
       ]
       variable = "token.actions.githubusercontent.com:sub"
     }
