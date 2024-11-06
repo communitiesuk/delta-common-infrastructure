999b94a572566a18aea416495572185c2dd40ad9
JamesMooreDLUHC <james.moore1@levellingup.gov.uk>
2024-11-06 14:31:01 +0000

    DT-280

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -321,7 +321,7 @@ module "marklogic" {
   backup_replication_bucket               = module.backup_replication_bucket.bucket
   ebs_backup_role_arn                     = module.ebs_backup.role_arn
   ebs_backup_completed_sns_topic_arn      = module.ebs_backup.sns_topic_arn
-  iam_github_openid_connect_provider_arn  = data.aws_iam_openid_connect_provider.github.arn,
+  iam_github_openid_connect_provider_arn  = data.aws_iam_openid_connect_provider.github.arn
   ses_deploy_secret_arns                  = [module.delta_ses_user.deploy_secret_arn, module.cpm_ses_user.deploy_secret_arn]
 }
 
