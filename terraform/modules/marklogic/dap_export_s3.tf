4dbf076460bf7d78ec6b694e4cd8651d2ab5a9e3
keithkennedy <keith@kennedy.dev>
2025-01-13 11:54:16 +0000

    DT-1395: Sharing staging S3 DAP bucket with Funding Service Find team

--- terraform/modules/marklogic/dap_export_s3.tf
+++ terraform/modules/marklogic/dap_export_s3.tf
@@ -32,7 +32,7 @@ data "aws_iam_policy_document" "allow_access_from_dap" {
     ]
   }
   statement {
-    sid = "AllowS3BucketLogs"
+    sid = "AllowExternalBucketAccess"
     effect = "Allow"
     principals {
       type        = "CanonicalUser"
