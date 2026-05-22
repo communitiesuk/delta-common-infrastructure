6aeacd8058f3b506b81c07f25de475dd2352ac89
Keith Kennedy <keith.kennedy@communities.gov.uk>
2026-05-22 08:40:08 +0100

    Consolidated TF providers across repo (#586)

---
+++ terraform/modules/provider_versions/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.100.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.8.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.7.1"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.1.0"
+    }
+  }
+
+  required_version = "~> 1.9.0"
+}
