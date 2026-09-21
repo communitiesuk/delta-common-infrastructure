322a21a69936e26951c4cdb59824ddeb5b1ad281
Yousuf Desai <119860263+YD-MHCLG@users.noreply.github.com>
2026-09-21 11:41:43 +0100

    Update terraform/modules/marklogic/variables.tf
    Co-authored-by: Keith Kennedy <keith.kennedy@communities.gov.uk>

--- terraform/modules/marklogic/variables.tf
+++ terraform/modules/marklogic/variables.tf
@@ -124,7 +124,7 @@ variable "marklogic_ami_version" {
   type = string
 
   validation {
-    condition     = var.marklogic_ami_version == "10.0-10.2" || var.marklogic_ami_version == "10.0-9.5" || var.marklogic_ami_version == "11.3.7"
+    condition     = var.marklogic_ami_version == "11.3.7"
     error_message = "Only specific versions allowed, configure AMIs for others"
   }
 }
